eb5de4347db2e3ff8137cb9ec55d87511e317172
Ingo Köpp <ingokpp@gmail.com>
2017-11-18 22:08:51 +0100

    Added right profile page to the tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,3 +1,4 @@
+import { ProfilePage } from './../profile/profile';
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
@@ -12,6 +13,7 @@ export class TabsPage {
   tab1Root = DashboardPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = ProfilePage;
 
   constructor() {
 
