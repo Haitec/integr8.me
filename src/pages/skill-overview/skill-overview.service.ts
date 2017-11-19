4ee9b322d40a00874955bcea3b660fc854b51a14
Ingo Köpp <ingokpp@gmail.com>
2017-11-19 11:23:21 +0100

    skill overview

---
+++ src/pages/skill-overview/skill-overview.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import { BaseInput } from "ionic-angular/util/base-input";
+
+const BASE_URL = "/api/skills/";
+const USER_ID = 1;
+
+@Injectable()
+export class SkillOverviewService {
+  constructor(private http: Http) {}
+
+  get(id) {
+    return this.http.get(BASE_URL + id + "?user=" + USER_ID).toPromise();
+  }
+
+  favourite(isFavourite: boolean = true, jobId: number) {
+    const fav = isFavourite ? "1" : "0";
+    return this.http
+      .get(`/api/job/${jobId}/favourite/${fav}?user=` + USER_ID)
+      .toPromise();
+  }
+}
