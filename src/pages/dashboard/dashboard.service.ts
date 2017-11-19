2bf882840d17d85e208e12aabfefb133ef1bf73e
Miguel Dönicke <miguel@doenicke.io>
2017-11-19 11:47:48 +0100

    changing favourite call url

--- src/pages/dashboard/dashboard.service.ts
+++ src/pages/dashboard/dashboard.service.ts
@@ -5,7 +5,7 @@ import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
 import { BaseInput } from "ionic-angular/util/base-input";
 
-const BASE_URL = "https://integr8-me.sloppy.zone/dashboard";
+const BASE_URL = "https://integr8-me.sloppy.zone";
 const USER_ID = 1;
 
 @Injectable()
@@ -13,13 +13,13 @@ export class DashboardService {
   constructor(private http: Http) {}
 
   get() {
-    return this.http.get(BASE_URL + "?user=" + USER_ID).toPromise();
+    return this.http.get(BASE_URL + "/dashboard?user=" + USER_ID).toPromise();
   }
 
   favourite(isFavourite: boolean = true, jobId: number) {
     const fav = isFavourite ? "1" : "0";
     return this.http
-      .get(`/api/job/${jobId}/favourite/${fav}?user=` + USER_ID)
+      .get(BASE_URL + `/job/${jobId}/favourite/${fav}?user=` + USER_ID)
       .toPromise();
   }
 }
